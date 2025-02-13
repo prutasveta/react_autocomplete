0bd59af1136201c7cb2ca2bd036369b6b8627ee5
prutasveta <svetapruta@gmail.com>
2025-02-13 13:03:26 +0100

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import { PeopleList } from './components/PeopleList';
+import { Person } from './types/Person';
+
+import debounce from 'lodash.debounce';
+import { event } from 'cypress/types/jquery';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const [people, setPeople] = useState(peopleFromServer);
+  // const [people, setPeople] = useState<Person[]>(getPreparedPeople());
+  const [isFocused, setIsFoused] = useState(false);
+  const [seletedPerson, setSeletedPerson] = useState<Person | null>(null);
+  const [query, setQuery] = useState('');
+  const [appliedQuery, setAppliedQuery] = useState('');
+
+  const filteredPeople = useMemo(() => {
+    return people.filter(person => person.name.includes(query));
+  }, [appliedQuery, people]);
+
+  const applyQuery = useCallback(debounce(setAppliedQuery, 300), []);
+
+  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSeletedPerson(null);
+    setQuery(event.target.value);
+    applyQuery(event.target.value);
+  };
+
+  const handleSelect = person => {
+    setSeletedPerson(person);
+    // setQuery(person.name);
+  };
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {seletedPerson
+            ? `${seletedPerson?.name} (${seletedPerson?.born} - ${seletedPerson?.died})`
+            : 'No selected person'}
+          {}
         </h1>
 
         <div className="dropdown is-active">
@@ -19,12 +49,23 @@ export const App: React.FC = () => {
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              value={query}
+              onFocus={() => setIsFoused(true)}
+              onBlur={() => setIsFoused(false)}
+              onChange={handleQueryChange}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
+            {isFocused && (
+              <PeopleList
+                people={filteredPeople}
+                onSelect={handleSelect}
+                key={seletedPerson?.name}
+              />
+            )}
+
+            {/* <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-link">Pieter Haverbeke</p>
               </div>
 
@@ -50,24 +91,25 @@ export const App: React.FC = () => {
 
               <div className="dropdown-item" data-cy="suggestion-item">
                 <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
+              </div> */}
           </div>
         </div>
 
-        <div
-          className="
+        {filteredPeople.length === 0 && (
+          <div
+            className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
